0180363e41f226123a3583c6476316c43ca5f8ab
Geoffrey Stewart <geoffreydstewart@hotmail.com>
2021-05-27 22:24:09 -0700

    export java_home and change opt

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ echo Building...
 ANT_OPTS=-Xmx384m
 #ANT_HOME=/usr/share/java/ant-1.8.4
 JAVA_HOME=/myopt/java/jdk1.8.0_162
-ANT_HOME=/opt/ant/apache-ant-1.9.2
+ANT_HOME=/myopt/ant/apache-ant-1.9.2
 
-export ANT_HOME
+export ANT_HOME JAVA_HOME
 $ANT_HOME/bin/ant -buildfile build_package.xml
 
 #export ANT_HOME
